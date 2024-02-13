8d9c598a3ad1251119badee813fca10942c892f8
Ahmad Kamal Nasir <ahmad.nasir@telematik-zentrum.de>
2024-02-14 03:09:25 +0500

    style: format code

--- mav_trajectory_generation_example/src/example_planner.cc
+++ mav_trajectory_generation_example/src/example_planner.cc
@@ -1,37 +1,35 @@
 #include <mav_trajectory_generation_example/example_planner.h>
 
-ExamplePlanner::ExamplePlanner(ros::NodeHandle& nh) :
-    nh_(nh),
-    max_v_(2.0),
-    max_a_(2.0),
-    current_velocity_(Eigen::Vector3d::Zero()),
-    current_pose_(Eigen::Affine3d::Identity()) {
-
+ExamplePlanner::ExamplePlanner(ros::NodeHandle& nh)
+  : nh_(nh)
+  , max_v_(2.0)
+  , max_a_(2.0)
+  , current_velocity_(Eigen::Vector3d::Zero())
+  , current_pose_(Eigen::Affine3d::Identity())
+{
   // Load params
-  if (!nh_.getParam(ros::this_node::getName() + "/max_v", max_v_)){
+  if (!nh_.getParam(ros::this_node::getName() + "/max_v", max_v_))
+  {
     ROS_WARN("[example_planner] param max_v not found");
   }
-  if (!nh_.getParam(ros::this_node::getName() + "/max_a", max_a_)){
+  if (!nh_.getParam(ros::this_node::getName() + "/max_a", max_a_))
+  {
     ROS_WARN("[example_planner] param max_a not found");
   }
 
   // create publisher for RVIZ markers
-  pub_markers_ =
-      nh.advertise<visualization_msgs::MarkerArray>("trajectory_markers", 0);
+  pub_markers_ = nh.advertise<visualization_msgs::MarkerArray>("trajectory_markers", 0);
 
-  pub_trajectory_ =
-      nh.advertise<mav_planning_msgs::PolynomialTrajectory4D>("trajectory",
-                                                              0);
+  pub_trajectory_ = nh.advertise<mav_planning_msgs::PolynomialTrajectory4D>("trajectory", 0);
 
   // subscriber for Odometry
-  sub_odom_ =
-      nh.subscribe("uav_pose", 1, &ExamplePlanner::uavOdomCallback, this);
+  sub_odom_ = nh.subscribe("uav_pose", 1, &ExamplePlanner::uavOdomCallback, this);
 }
 
 // Callback to get current Pose of UAV
-void ExamplePlanner::uavOdomCallback(const nav_msgs::Odometry::ConstPtr& odom) {
-
-  std::cout<< "done"<<std::endl;
+void ExamplePlanner::uavOdomCallback(const nav_msgs::Odometry::ConstPtr& odom)
+{
+  std::cout << "done" << std::endl;
   // store current position in our planner
   tf::poseMsgToEigen(odom->pose.pose, current_pose_);
 
@@ -40,17 +38,16 @@ void ExamplePlanner::uavOdomCallback(const nav_msgs::Odometry::ConstPtr& odom) {
 }
 
 // Method to set maximum speed.
-void ExamplePlanner::setMaxSpeed(const double max_v) {
+void ExamplePlanner::setMaxSpeed(const double max_v)
+{
   max_v_ = max_v;
 }
 
 // Plans a trajectory from the current position to the a goal position and velocity
 // we neglect attitude here for simplicity
-bool ExamplePlanner::planTrajectory(const Eigen::VectorXd& goal_pos,
-                                    const Eigen::VectorXd& goal_vel,
-                                    mav_trajectory_generation::Trajectory* trajectory) {
-
-
+bool ExamplePlanner::planTrajectory(const Eigen::VectorXd& goal_pos, const Eigen::VectorXd& goal_vel,
+                                    mav_trajectory_generation::Trajectory* trajectory)
+{
   // 3 Dimensional trajectory => through carteisan space, no orientation
   const int dimension = 3;
 
@@ -58,36 +55,29 @@ bool ExamplePlanner::planTrajectory(const Eigen::VectorXd& goal_pos,
   mav_trajectory_generation::Vertex::Vector vertices;
 
   // Optimze up to 4th order derivative (SNAP)
-  const int derivative_to_optimize =
-      mav_trajectory_generation::derivative_order::SNAP;
+  const int derivative_to_optimize = mav_trajectory_generation::derivative_order::SNAP;
 
   // we have 2 vertices:
   // Start = current position
   // end = desired position and velocity
   mav_trajectory_generation::Vertex start(dimension), end(dimension);
 
-
   /******* Configure start point *******/
   // set start point constraints to current position and set all derivatives to zero
-  start.makeStartOrEnd(current_pose_.translation(),
-                       derivative_to_optimize);
+  start.makeStartOrEnd(current_pose_.translation(), derivative_to_optimize);
 
   // set start point's velocity to be constrained to current velocity
-  start.addConstraint(mav_trajectory_generation::derivative_order::VELOCITY,
-                      current_velocity_);
+  start.addConstraint(mav_trajectory_generation::derivative_order::VELOCITY, current_velocity_);
 
   // add waypoint to list
   vertices.push_back(start);
 
-
   /******* Configure end point *******/
   // set end point constraints to desired position and set all derivatives to zero
-  end.makeStartOrEnd(goal_pos,
-                     derivative_to_optimize);
+  end.makeStartOrEnd(goal_pos, derivative_to_optimize);
 
   // set start point's velocity to be constrained to current velocity
-  end.addConstraint(mav_trajectory_generation::derivative_order::VELOCITY,
-                    goal_vel);
+  end.addConstraint(mav_trajectory_generation::derivative_order::VELOCITY, goal_vel);
 
   // add waypoint to list
   vertices.push_back(end);
@@ -118,20 +108,17 @@ bool ExamplePlanner::planTrajectory(const Eigen::VectorXd& goal_pos,
 }
 
 // Plans a trajectory from a start position and velocity to a goal position and velocity
-bool ExamplePlanner::planTrajectory(const Eigen::VectorXd& goal_pos,
-                                    const Eigen::VectorXd& goal_vel,
-                                    const Eigen::VectorXd& start_pos,
-                                    const Eigen::VectorXd& start_vel,
-                                    double v_max, double a_max,
-                                    mav_trajectory_generation::Trajectory* trajectory) {
+bool ExamplePlanner::planTrajectory(const Eigen::VectorXd& goal_pos, const Eigen::VectorXd& goal_vel,
+                                    const Eigen::VectorXd& start_pos, const Eigen::VectorXd& start_vel, double v_max,
+                                    double a_max, mav_trajectory_generation::Trajectory* trajectory)
+{
   assert(trajectory);
   const int dimension = goal_pos.size();
   // Array for all waypoints and their constraints
   mav_trajectory_generation::Vertex::Vector vertices;
 
   // Optimze up to 4th order derivative (SNAP)
-  const int derivative_to_optimize =
-      mav_trajectory_generation::derivative_order::SNAP;
+  const int derivative_to_optimize = mav_trajectory_generation::derivative_order::SNAP;
 
   // we have 2 vertices:
   // start = desired start vector
@@ -140,16 +127,14 @@ bool ExamplePlanner::planTrajectory(const Eigen::VectorXd& goal_pos,
 
   /******* Configure start point *******/
   start.makeStartOrEnd(start_pos, derivative_to_optimize);
-  start.addConstraint(mav_trajectory_generation::derivative_order::VELOCITY,
-                      start_vel);
+  start.addConstraint(mav_trajectory_generation::derivative_order::VELOCITY, start_vel);
   vertices.push_back(start);
 
   /******* Configure end point *******/
   // set end point constraints to desired position and set all derivatives to zero
   end.makeStartOrEnd(goal_pos, derivative_to_optimize);
-  end.addConstraint(mav_trajectory_generation::derivative_order::VELOCITY,
-                    goal_vel);
-vertices.push_back(end);
+  end.addConstraint(mav_trajectory_generation::derivative_order::VELOCITY, goal_vel);
+  vertices.push_back(end);
 
   // setimate initial segment times
   std::vector<double> segment_times;
@@ -177,26 +162,21 @@ vertices.push_back(end);
   return true;
 }
 
-bool ExamplePlanner::publishTrajectory(const mav_trajectory_generation::Trajectory& trajectory){
+bool ExamplePlanner::publishTrajectory(const mav_trajectory_generation::Trajectory& trajectory)
+{
   // send trajectory as markers to display them in RVIZ
   visualization_msgs::MarkerArray markers;
-  double distance =
-      0.2; // Distance by which to seperate additional markers. Set 0.0 to disable.
+  double distance = 0.2;  // Distance by which to seperate additional markers. Set 0.0 to disable.
   std::string frame_id = "world";
 
-  mav_trajectory_generation::drawMavTrajectory(trajectory,
-                                               distance,
-                                               frame_id,
-                                               &markers);
+  mav_trajectory_generation::drawMavTrajectory(trajectory, distance, frame_id, &markers);
   pub_markers_.publish(markers);
 
   // send trajectory to be executed on UAV
   mav_planning_msgs::PolynomialTrajectory msg;
-  mav_trajectory_generation::trajectoryToPolynomialTrajectoryMsg(trajectory,
-                                                                 &msg);
+  mav_trajectory_generation::trajectoryToPolynomialTrajectoryMsg(trajectory, &msg);
   msg.header.frame_id = "world";
   pub_trajectory_.publish(msg);
 
   return true;
 }
-

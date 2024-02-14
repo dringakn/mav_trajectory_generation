a69b2bae5ba5715f24731c4af1f9809f006efaaf
Ahmad Kamal Nasir <ahmad.nasir@telematik-zentrum.de>
2024-02-14 05:51:05 +0500

    feat: publish trajectory

--- mav_trajectory_generation_example/src/example_evaluate_plan.cpp
+++ mav_trajectory_generation_example/src/example_evaluate_plan.cpp
@@ -1,11 +1,16 @@
-#include <ros/ros.h>
+// #include <ros/ros.h>
+#include "ros/ros.h"
 #include <nav_msgs/Odometry.h>
 #include <nav_msgs/Path.h>
 #include <geometry_msgs/Twist.h>
+#include <visualization_msgs/MarkerArray.h>
 
-// Write the planne trajector function.
-// Provide it to the planner.
-// Publish the trajectory for rviz to display.
+#include "mav_trajectory_generation/polynomial_optimization_linear.h"
+#include "mav_trajectory_generation/polynomial_optimization_nonlinear.h"
+#include "mav_trajectory_generation/trajectory.h"
+#include <mav_trajectory_generation_ros/ros_visualization.h>
+#include <mav_trajectory_generation_ros/ros_conversions.h>
+
 // implement the controller to generate V/W for diff robot gazebo simulator.
 // evaluate the plan and show the results.
 // use teleop to mix the inputs.
@@ -18,7 +23,11 @@ public:
     odom_sub_ = nh_.subscribe("odometry", 10, &MyNode::odomCallback, this);
     cmd_vel_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 10);
     path_pub_ = nh_.advertise<nav_msgs::Path>("waypoints", 10, true);
+    trajectory_pub_ = nh_.advertise<mav_planning_msgs::PolynomialTrajectory4D>("trajectory", 10, true);
+    markers_pub_ = nh_.advertise<visualization_msgs::MarkerArray>("trajectory_markers", 10, true);
 
+    max_linear_velocity = 1.0;
+    max_linear_acceleration = 1.0;
     initialize_edges();
     polygon_vertices_.push_back(Point{ -10, -10 });
     polygon_vertices_.push_back(Point{ 10, -10 });
@@ -45,7 +54,16 @@ public:
       path.poses.push_back(pose);
     }
     path_pub_.publish(path);
-    std::cout << "Waypoints:" << path.poses.size() << std::endl;
+
+    if (generate_plan(path))
+    {
+      std::cout << "Waypoints:" << path.poses.size() << std::endl;
+      publishTrajectory(trajectory);
+    }
+    else
+    {
+      std::cout << "Failed to generate plan." << std::endl;
+    }
   }
 
   void odomCallback(const nav_msgs::Odometry::ConstPtr& odom_msg)
@@ -71,12 +89,21 @@ public:
   }
 
 private:
+  static const int DIM = 4;   // Define the dimension of the trajectory (3/4D).
+  static const int N6 = 6;    // Define the order for polynomial optimization for boustrophedon planner.
+  static const int N10 = 10;  // Define the order for polynomial optimization for relaxed planner.
+
   ros::NodeHandle nh_;
   ros::Rate loop_rate_;
   nav_msgs::Odometry odom_msg_;
   nav_msgs::Path path;
   ros::Subscriber odom_sub_;
-  ros::Publisher cmd_vel_pub_, path_pub_;
+  ros::Publisher cmd_vel_pub_, path_pub_, trajectory_pub_, markers_pub_;
+
+  mav_trajectory_generation::Trajectory trajectory;             // Output trajectory
+  mav_trajectory_generation::Vertex::Vector waypoint_vertices;  // List of vertices for trajectory generation.
+  std::vector<double> segment_times;                            // List to estimate initial waypoints segment times.
+  double max_linear_velocity, max_linear_acceleration;
 
   struct Point
   {
@@ -103,6 +130,105 @@ private:
       edge.end = polygon_vertices_[(i + 1) % polygon_vertices_.size()];
       polygon_edges_.push_back(edge);
     }
+  }
+
+  bool generate_plan(const nav_msgs::Path& waypoints)
+  {
+    if (waypoints.poses.size() < 2)
+    {
+      std::cout << "Input path contains less than 2 points. " << std::endl;
+      return false;
+    }
+
+    trajectory.clear();
+    waypoint_vertices.clear();
+    segment_times.clear();
+
+    if (!waypoints.poses.empty())
+    {
+      auto&& first_wp = waypoints.poses.front();
+      mav_trajectory_generation::Vertex first_vtx(DIM);
+      first_vtx.makeStartOrEnd(
+          Eigen::Vector4d(first_wp.pose.position.x, first_wp.pose.position.y, first_wp.pose.position.z, 0),
+          mav_trajectory_generation::derivative_order::SNAP);
+      waypoint_vertices.push_back(first_vtx);
+
+      for (auto it = std::next(waypoints.poses.begin()); it != std::prev(waypoints.poses.end()); ++it)
+      {
+        auto&& wp = *it;
+        mav_trajectory_generation::Vertex vtx(DIM);
+        vtx.addConstraint(mav_trajectory_generation::derivative_order::POSITION,
+                          Eigen::Vector4d(wp.pose.position.x, wp.pose.position.y, wp.pose.position.z, 0));
+        waypoint_vertices.push_back(vtx);
+      }
+
+      auto&& last_wp = waypoints.poses.back();
+      mav_trajectory_generation::Vertex last_vtx(DIM);
+      last_vtx.makeStartOrEnd(
+          Eigen::Vector4d(last_wp.pose.position.x, last_wp.pose.position.y, last_wp.pose.position.z, 0),
+          mav_trajectory_generation::derivative_order::SNAP);
+      waypoint_vertices.push_back(last_vtx);
+    }
+
+    segment_times = mav_trajectory_generation::estimateSegmentTimes(waypoint_vertices, max_linear_velocity,
+                                                                    max_linear_acceleration);
+
+    mav_trajectory_generation::NonlinearOptimizationParameters optimizerParameters;
+    // optimizerParameters.max_iterations = 1000;
+    // optimizerParameters.f_rel = 0.05;
+    // optimizerParameters.x_rel = 0.1;
+    // optimizerParameters.time_penalty = 500.0;
+    // optimizerParameters.initial_stepsize_rel = 0.1;
+    // optimizerParameters.inequality_constraint_tolerance = 0.1;
+    // optimizerParameters.print_debug_info = true;
+    // optimizerParameters.print_debug_info_time_allocation = true;
+    // optimizerParameters.use_soft_constraints = true;
+    // optimizerParameters.soft_constraint_weight = 100;
+
+    /*  SETUP OPTIMIZER  */
+    mav_trajectory_generation::PolynomialOptimizationNonLinear<N10> optimizer(DIM, optimizerParameters);
+    optimizer.setupFromVertices(waypoint_vertices, segment_times, mav_trajectory_generation::derivative_order::SNAP);
+    // optimizer.addMaximumMagnitudeConstraint(mav_trajectory_generation::derivative_order::ANGULAR_ACCELERATION,
+    // DEG2RAD(max_angular_acceleration));
+    // optimizer.addMaximumMagnitudeConstraint(mav_trajectory_generation::derivative_order::ANGULAR_VELOCITY,
+    // DEG2RAD(max_angular_velocity));
+    optimizer.addMaximumMagnitudeConstraint(mav_trajectory_generation::derivative_order::VELOCITY, max_linear_velocity);
+    optimizer.addMaximumMagnitudeConstraint(mav_trajectory_generation::derivative_order::ACCELERATION,
+                                            max_linear_acceleration);
+
+    auto startTime = ros::Time::now();  // Start the timer
+    optimizer.optimize();
+    auto endTime = ros::Time::now();  // Stop the timer
+    auto elapsedTime = (endTime - startTime).toSec();
+    std::cout << "Optimization took " << elapsedTime << " seconds" << std::endl;
+    optimizer.getTrajectory(&trajectory);
+    // trajectory.scaleSegmentTimesToMeetConstraints(max_linear_velocity, max_linear_acceleration);
+
+    if (trajectory.empty())
+    {
+      return false;
+    }
+
+    return true;
+  }
+
+  bool publishTrajectory(const mav_trajectory_generation::Trajectory& trajectory)
+  {
+    // send trajectory as markers to display them in RVIZ
+    visualization_msgs::MarkerArray markers;
+    double distance = 0.2;  // Distance by which to seperate additional markers. Set 0.0 to disable.
+    std::string frame_id = "odom";
+
+    mav_trajectory_generation::drawMavTrajectory(trajectory, distance, frame_id, &markers);
+    markers_pub_.publish(markers);
+
+    // send trajectory to be executed on UAV
+    mav_planning_msgs::PolynomialTrajectory msg;
+    mav_trajectory_generation::trajectoryToPolynomialTrajectoryMsg(trajectory, &msg);
+    msg.header.frame_id = "odom";
+    trajectory_pub_.publish(msg);
+
+    return true;
   }
 };
 
